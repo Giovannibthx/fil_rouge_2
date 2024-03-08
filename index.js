d53e799bd654a3f1a068a2a540356f45157f7d2e
Giovanni <giovanni.bthx@gmail.com>
2024-03-08 14:32:53 +0100

    chore : set cors to expose headers

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+    cors({
+        exposedHeaders: ["auth-token"],
+    })
+);
 
 app.use(
     bodyParser.urlencoded({
@@ -22,7 +26,7 @@ app.use(
 app.use(express.json());
 app.use(
     express.urlencoded({
-        axtended: true,
+        extended: true,
     })
 );
 
